841a21dd293a026327c7c0af7166087cc90341c7
Philippa Karoly <pkaroly@student.unimelb.edu.au>
2014-10-24 16:39:36 +1100

    .

--- src/sigmoid_JR.m
+++
@@ -1,8 +0,0 @@
-
-% note, the maximum firing rate is lumped into synaptic gain
-
-function g = sigmoid_JR(varsigma,v0,v)
-
-%g = 1./(1 + exp(varsigma*(v0 - v)));% - 1./(1 + exp(varsigma*v0));      % note, the maximum firing rate is lumped into synaptic gain
-g = 0.5*erf((v - v0) / (sqrt(2)*varsigma)) + 0.5;
-
